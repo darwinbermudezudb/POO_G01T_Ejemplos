ff76a3a1403caacbf2e8633ea457c3165d8b3d30
Darwin Bermudez <jose.bermudez@docente.udb.edu.sv>
2024-07-15 13:16:22 -0600

    Generacion de carnet

---
+++ Semana4/src/Alumno.java
@@ -0,0 +1,13 @@
+public class Alumno {
+
+    String nombre = "";
+    String primer_apellido = "";
+    String segundo_apellido = "";
+    String carnet = "";
+
+    public String generarCarnet(String carnet){
+        return this.carnet = carnet;
+    }
+
+
+}
