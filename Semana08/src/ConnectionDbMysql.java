271cde347d74fc27bcfed84a46326774155e4817
Darwin Bermudez <95990676+darwinbermudez27@users.noreply.github.com>
2024-08-26 10:17:58 -0600

    CRUD de usuario

--- Semana08/src/ConnectionDbMysql.java
+++ Semana08/src/ConnectionDbMysql.java
@@ -6,34 +6,56 @@ public class ConnectionDbMysql {
     private int puerto;
     private String db_name;
     private String clave;
+    private Connection conn;
     public Statement stmt;
+    private PreparedStatement pstmt;
 
     public ConnectionDbMysql(){
         this.dominio = "jdbc:mysql://localhost";
         this.puerto = 8889;
-        this.usuario = "root";
         this.db_name = "miBD";
+        this.usuario = "root";
         this.clave = "abc123";
+
+        this.connet();
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
     }
 
     public void connet(){
         try{
             String stringconnection = this.dominio + ":" + this.puerto + "/" + this.db_name;
-            Connection conn = DriverManager.getConnection(stringconnection, this.usuario, this.clave);
-            this.stmt = conn.createStatement();
+            this.conn = DriverManager.getConnection(stringconnection, this.usuario, this.clave);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
 
-    public void consultar(String query) throws SQLException {
+    public ResultSet consultar(String query) throws SQLException {
+        this.stmt = conn.createStatement();
         ResultSet rs = this.stmt.executeQuery(query);
-        while (rs.next()){
-            int id = rs.getInt("id");
-            String nombre = rs.getString("nombre");
-            int edad = rs.getInt("edad");
-
-            System.out.println("El identificador es: " + id + ", el nombre es: " + nombre + " y la edad esL " + edad);
-        }
+        return rs;
     }
+
+    public PreparedStatement insert(String query) throws SQLException {
+        this.pstmt = conn.prepareStatement(query);
+        return this.pstmt;
+    }
+
+    public PreparedStatement update(String query) throws SQLException {
+        this.pstmt = conn.prepareStatement(query);
+        return this.pstmt;
+    }
+
+    public PreparedStatement delete(String query) throws SQLException {
+        this.pstmt = conn.prepareStatement(query);
+        return this.pstmt;
+    }
+
 }
--- Semana08/src/Main.java
+++ Semana08/src/Main.java
@@ -5,7 +5,52 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         ConnectionDbMysql conn = new ConnectionDbMysql();;
-        conn.connet();
-        conn.consultar("\"SELECT id, nombre, edad FROM usuarios\"");
+
+
+        // consulta de datos
+        ResultSet usuario_consultar = conn.consultar("SELECT id, nombre, edad FROM usuarios");
+        while (usuario_consultar.next()){
+            int id = usuario_consultar.getInt("id");
+            String nombre = usuario_consultar.getString("nombre");
+            int edad = usuario_consultar.getInt("edad");
+            System.out.println("El resultado de usuario es: El identificador es: " + id + ", el nombre es: " + nombre + " y la edad esL " + edad);
+        }
+
+        /*
+        // consulta de datos
+        ResultSet cliente_consultar = conn.consultar("SELECT id, nombre, apellido, edad FROM cliente");
+        while (cliente_consultar.next()){
+            int id = cliente_consultar.getInt("id");
+            String nombre = cliente_consultar.getString("nombre");
+            String apellido = cliente_consultar.getString("apellido");
+            int edad = cliente_consultar.getInt("edad");
+            System.out.println("El resultado de cliente es: El identificador es: " + id + ", el nombre es: " + nombre + ", el apellido es: "  + apellido + " y la edad esL " + edad);
+        }
+    */
+
+        // Insert
+        PreparedStatement usuario_insertar = conn.insert("INSERT INTO usuarios (nombre, edad) VALUES (?, ?)");
+        usuario_insertar.setString(1, "Oscar");
+        usuario_insertar.setInt(2, 20);
+        usuario_insertar.executeUpdate();
+
+        PreparedStatement usuario_actualizacion = conn.update("UPDATE usuarios SET nombre = ? WHERE id = ?");
+        usuario_actualizacion.setString(1, "Alexandra");
+        usuario_actualizacion.setInt(2, 6);
+        int rowsAffected = usuario_actualizacion.executeUpdate();
+askjdlahsldkhalsh
+        PreparedStatement usuario_borrrar = conn.delete("Delete FROM usuarios");
+        usuario_borrrar.executeUpdate();
+
+
+        usuario_consultar = conn.consultar("SELECT id, nombre, edad FROM usuarios");
+        while (usuario_consultar.next()){
+            int id = usuario_consultar.getInt("id");
+            String nombre = usuario_consultar.getString("nombre");
+            int edad = usuario_consultar.getInt("edad");
+            System.out.println("El resultado de usuario es: El identificador es: " + id + ", el nombre es: " + nombre + " y la edad esL " + edad);
+        }
+
+
     }
 }
